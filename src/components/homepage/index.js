bb749015c00939e5c9661d976fdcc0cf59c34685
Alex Martin <atonymartin20@yahoo.com>
2021-06-15 22:47:30 -0500

    Starting to add variables for filter dropdown menu

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -6,17 +6,38 @@ import '../../css/homepage.css';
 import InvoicesHeader from './invoicesHeader.js';
 
 class Homepage extends React.Component {
+    state = {
+        filterStatusContainerOpen: false,
+        draftFilter: false,
+        paidFilter: false,
+        pendingFilter: false,
+    }
+
+    openFilterStatusMenu = (event) => {
+        event.preventDefault();
+        this.setState({
+            filterStatusContainerOpen: true
+        })
+    }
+
+    filterByStatusDraft = (event) => {
+        event.preventDefault();
+        this.setState({
+            draftFilter: !this.state.draftFilter
+        })
+    }
+
     render() {
         return(
             <div className='homepage-container-div'>
                 <Navbar />
                 {this.context.state.darkMode === true ?
                     <div className='homepage-inside-container-div-dark-mode'>
-                        <InvoicesHeader type='dark' />
+                        <InvoicesHeader type='dark' openFilterStatusMenu={this.openFilterStatusMenu} filterStatusContainerOpen={this.state.filterStatusContainerOpen} />
                     </div>
                 :
                     <div className='homepage-inside-container-div'>
-                        <InvoicesHeader type='light' />
+                        <InvoicesHeader type='light' openFilterStatusMenu={this.openFilterStatusMenu} filterStatusContainerOpen={this.state.filterStatusContainerOpen} />
                     </div>
                 }
             </div>
