e10c081680cd03faa883eec8e4e1bef0fd268f60
Alex Martin <atonymartin20@yahoo.com>
2021-07-07 12:03:03 -0500

    Updated invoicesContainer for darkMode

--- src/components/showInvoices/invoicesContainer.js
+++ src/components/showInvoices/invoicesContainer.js
@@ -12,7 +12,71 @@ class InvoicesContainer extends React.Component {
 		return (
 			<div className="invoices-container-div">
 				{this.props.type === 'dark' ? (
-					<div className="dark-mode">
+					<div className="invoices-inside-container-div">
+						                        {this.props.activeFilter === false ? 
+                            <div className='card-container-div'>
+                                {this.context.state.invoices.map(invoice => (
+									<InvoiceCard
+										key={invoice.id}
+										id={invoice.id}
+										paymentDue={invoice.paymentDue}
+										clientName={invoice.clientName}
+										total={invoice.total}
+										status={invoice.status}
+									/>
+								))}
+                            </div>
+                        : 
+                            null
+                        } 
+
+						{this.props.activeFilter === true && this.props.draftFilter === true ? 
+                            <div>
+                                {this.context.state.draftInvoices.map(invoice => (
+									<InvoiceCard
+										id={invoice.id}
+										paymentDue={invoice.paymentDue}
+										clientName={invoice.clientName}
+										total={invoice.total}
+										status={invoice.status}
+									/>
+								))}
+                            </div>
+                        : 
+                            null
+                        } 
+
+						{this.props.activeFilter === true && this.props.pendingFilter === true ? 
+                            <div>
+                                {this.context.state.pendingInvoices.map(invoice => (
+									<InvoiceCard
+										id={invoice.id}
+										paymentDue={invoice.paymentDue}
+										clientName={invoice.clientName}
+										total={invoice.total}
+										status={invoice.status}
+									/>
+								))}
+                            </div>
+                        : 
+                            null
+                        } 
+
+						{this.props.activeFilter === true && this.props.paidFilter === true ? 
+                            <div>
+                                {this.context.state.paidInvoices.map(invoice => (
+									<InvoiceCard
+										id={invoice.id}
+										paymentDue={invoice.paymentDue}
+										clientName={invoice.clientName}
+										total={invoice.total}
+										status={invoice.status}
+									/>
+								))}
+                            </div>
+                        : 
+                            null
+                        } 
 					</div>
 				) : (
 					<div className="invoices-inside-container-div">
