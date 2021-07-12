2cbe4a46bfb77ee0604af99a8005d507d56991a9
Alex Martin <atonymartin20@yahoo.com>
2021-07-11 23:52:43 -0500

    Beginning to add in an option if there are no invoices

--- src/components/showInvoices/invoiceCard.js
+++ src/components/showInvoices/invoiceCard.js
@@ -28,13 +28,29 @@ class InvoiceCard extends React.Component {
         })
     }
 	render() {
-		return (
-			<div className="card-outside-div">
-				{this.props.type === 'dark' ? (
-					<div className="dark-mode">
-					</div>
-				) : (
-					<div className="card-inside-div">
+
+        if (this.props.type === 'unfiltered' && this.props.colorType === 'dark') {
+            return (
+                <div className="card-outside-div">
+                    <div className="card-inside-div-dark">
+                        <span className='id-span'>#<span className='white-font-span'>{this.props.id}</span></span>
+                        <span className='due-date-span-dark-mode'>Due {this.state.day.toString().padStart(2,0)} {this.state.months[this.state.month]} {this.state.year}</span>
+                        <span className='client-name-span-dark-mode'>{this.props.clientName}</span>
+                        <span className='totals-span-dark-mode'>£ {this.state.total}</span>
+                        {this.props.status === 'draft' ? <div className='draft-status-div-dark-mode'><span className='status-span-draft-dark-mode'><div className='draft-ball-div-dark-mode' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'pending' ? <div className='pending-status-div'><span className='status-span-pending'><div className='pending-ball-div' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'paid' ? <div className='paid-status-div'><span className='status-span-paid'><div className='paid-ball-div' />{this.props.status}</span></div> : null }
+                        
+                        <img src={RightArrowIcon} alt='right arrow' />
+                    </div>
+                </div>
+            );
+        }
+
+        else if (this.props.type === 'unfiltered' && this.props.colorType === 'light') {
+            return (
+                <div className="card-outside-div">
+                    <div className="card-inside-div">
                         <span className='id-span'>#<span className='black-font-span'>{this.props.id}</span></span>
                         <span className='due-date-span'>Due {this.state.day.toString().padStart(2,0)} {this.state.months[this.state.month]} {this.state.year}</span>
                         <span className='client-name-span'>{this.props.clientName}</span>
@@ -44,10 +60,46 @@ class InvoiceCard extends React.Component {
                         {this.props.status === 'paid' ? <div className='paid-status-div'><span className='status-span-paid'><div className='paid-ball-div' />{this.props.status}</span></div> : null }
                         
                         <img src={RightArrowIcon} alt='right arrow' />
-					</div>
-				)}
-			</div>
-		);
+                    </div>
+                </div>
+            );
+        }
+
+        else if (this.props.type === 'filtered' && this.props.colorType === 'dark') {
+            return (
+                <div className="card-outside-div-filtered">
+                    <div className="card-inside-div-dark">
+                        <span className='id-span'>#<span className='white-font-span'>{this.props.id}</span></span>
+                        <span className='due-date-span-dark-mode'>Due {this.state.day.toString().padStart(2,0)} {this.state.months[this.state.month]} {this.state.year}</span>
+                        <span className='client-name-span-dark-mode'>{this.props.clientName}</span>
+                        <span className='totals-span-dark-mode'>£ {this.state.total}</span>
+                        {this.props.status === 'draft' ? <div className='draft-status-div-dark-mode'><span className='status-span-draft-dark-mode'><div className='draft-ball-div-dark-mode' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'pending' ? <div className='pending-status-div'><span className='status-span-pending'><div className='pending-ball-div' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'paid' ? <div className='paid-status-div'><span className='status-span-paid'><div className='paid-ball-div' />{this.props.status}</span></div> : null }
+                        
+                        <img src={RightArrowIcon} alt='right arrow' />
+                    </div>
+                </div>
+            );
+        }
+
+        else {
+            return (
+                <div className="card-outside-div-filtered">
+                    <div className="card-inside-div">
+                        <span className='id-span'>#<span className='black-font-span'>{this.props.id}</span></span>
+                        <span className='due-date-span'>Due {this.state.day.toString().padStart(2,0)} {this.state.months[this.state.month]} {this.state.year}</span>
+                        <span className='client-name-span'>{this.props.clientName}</span>
+                        <span className='totals-span'>£ {this.state.total}</span>
+                        {this.props.status === 'draft' ? <div className='draft-status-div'><span className='status-span-draft'><div className='draft-ball-div' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'pending' ? <div className='pending-status-div'><span className='status-span-pending'><div className='pending-ball-div' />{this.props.status}</span></div>: null }
+                        {this.props.status === 'paid' ? <div className='paid-status-div'><span className='status-span-paid'><div className='paid-ball-div' />{this.props.status}</span></div> : null }
+                        
+                        <img src={RightArrowIcon} alt='right arrow' />
+                    </div>
+                </div>
+            );
+        }
 	}
 }
 
