100848269fe9f3da3716163b658b15cc79382831
Alex Martin <atonymartin20@yahoo.com>
2021-07-11 23:53:08 -0500

    added type and colorType variables

--- src/components/showInvoices/invoicesContainer.js
+++ src/components/showInvoices/invoicesContainer.js
@@ -9,70 +9,109 @@ class InvoicesContainer extends React.Component {
 	};
 
 	render() {
+		console.log(this.context.state.invoices.length > 0)
+
 		return (
 			<div className="invoices-container-div">
 				{this.props.type === 'dark' ? (
 					<div className="invoices-inside-container-div">
-						                        {this.props.activeFilter === false ? 
-                            <div className='card-container-div'>
-                                {this.context.state.invoices.map(invoice => (
-									<InvoiceCard
-										key={invoice.id}
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
-                            </div>
+						{this.props.activeFilter === false ? 
+							<>
+								{this.context.state.invoices.length > 0 ? 
+		                            <div className='card-container-div-all-invoices'>
+										{this.context.state.invoices.map(invoice => (
+											<InvoiceCard
+												key={invoice.id}
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='unfiltered'
+												colorType='dark'
+											/>
+										))}
+									</div>
+								: 
+									<div>
+									</div>
+								}
+							</>
+
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.draftFilter === true ? 
-                            <div>
-                                {this.context.state.draftInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.draftInvoices.length > 0 ? 
+									<>
+										{this.context.state.draftInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='dark'
+											/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.pendingFilter === true ? 
-                            <div>
-                                {this.context.state.pendingInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.pendingInvoices.length > 0 ? 
+									<>
+										{this.context.state.pendingInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='dark'
+											/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.paidFilter === true ? 
-                            <div>
-                                {this.context.state.paidInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.paidInvoices.length > 0 ? 
+									<>
+										{this.context.state.paidInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='dark'
+											/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
@@ -80,66 +119,103 @@ class InvoicesContainer extends React.Component {
 					</div>
 				) : (
 					<div className="invoices-inside-container-div">
-                        {this.props.activeFilter === false ? 
-                            <div className='card-container-div'>
-                                {this.context.state.invoices.map(invoice => (
-									<InvoiceCard
-										key={invoice.id}
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
-                            </div>
+						{this.props.activeFilter === false ? 
+							<>
+								{this.context.state.invoices.length > 0 ? 
+		                            <div className='card-container-div-all-invoices'>
+										{this.context.state.invoices.map(invoice => (
+											<InvoiceCard
+												key={invoice.id}
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='unfiltered'
+												colorType='light'
+											/>
+										))}
+									</div>
+								: 
+									<div className='empty-invoices-container-div'>
+									</div>
+								}
+							</>
+
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.draftFilter === true ? 
-                            <div>
-                                {this.context.state.draftInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.draftInvoices.length > 0 ? 
+									<>
+										{this.context.state.draftInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='light'
+												/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.pendingFilter === true ? 
-                            <div>
-                                {this.context.state.pendingInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.pendingInvoices.length > 0 ? 
+									<>
+										{this.context.state.pendingInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='light'
+												/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
                         } 
 
 						{this.props.activeFilter === true && this.props.paidFilter === true ? 
-                            <div>
-                                {this.context.state.paidInvoices.map(invoice => (
-									<InvoiceCard
-										id={invoice.id}
-										paymentDue={invoice.paymentDue}
-										clientName={invoice.clientName}
-										total={invoice.total}
-										status={invoice.status}
-									/>
-								))}
+                            <div className='card-container-div-filtered-invoices'>
+								{this.context.state.paidInvoices.length > 0 ? 
+									<>
+										{this.context.state.paidInvoices.map(invoice => (
+											<InvoiceCard
+												id={invoice.id}
+												paymentDue={invoice.paymentDue}
+												clientName={invoice.clientName}
+												total={invoice.total}
+												status={invoice.status}
+												type='filtered'
+												colorType='light'
+												/>
+										))}
+									</>
+								: 
+									<div>
+									</div>
+								}
                             </div>
                         : 
                             null
