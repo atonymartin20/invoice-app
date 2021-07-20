def3f5587f60492814e23f6165478c4d26cda493
Alex Martin <atonymartin20@yahoo.com>
2021-07-19 20:15:33 -0500

    Styled left side of options bar

--- src/components/viewInvoice/index.js
+++ src/components/viewInvoice/index.js
@@ -35,8 +35,22 @@ class ViewInvoice extends React.Component {
 				) : (
 					<div className="view-invoice-outside-container-div">
 						<div className='view-invoice-inside-container-div'>
-							<span className='' onClick={this.handleRedirect}><img src={LeftArrowIcon} alt='left arrow' /> Go Back</span>
+							<span className='go-back-span' onClick={this.handleRedirect}><img src={LeftArrowIcon} alt='left arrow' className='left-arrow-icon' /> Go back</span>
 							
+							<div className='view-invoices-option-bar'>
+								<div className='view-invoices-option-bar-left-side'>
+									<span>Status </span>
+										{this.state.invoice.status === 'draft' ? <div className='draft-status-div'><span className='status-span-draft'><div className='draft-ball-div' />{this.state.invoice.status}</span></div>: null }
+										{this.state.invoice.status === 'pending' ? <div className='pending-status-div'><span className='status-span-pending'><div className='pending-ball-div' />{this.state.invoice.status}</span></div>: null }
+										{this.state.invoice.status === 'paid' ? <div className='paid-status-div'><span className='status-span-paid'><div className='paid-ball-div' />{this.state.invoice.status}</span></div> : null }
+								</div>
+
+								<div className='view-invoices-option-bar-right-side'>
+								</div>
+							</div>
+
+							<div className='view-invoices-info-div'>
+							</div>
 						</div>
 						
 
